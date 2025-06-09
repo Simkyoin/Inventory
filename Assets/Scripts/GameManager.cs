c5bf17107a1b83acb3cf479a118bb14aa51b2da6
Simkyoin <epahslr666@naver.com>
2025-06-10 08:59:16 +0900

    feat.아이템데이터

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,39 @@
 using UnityEngine;
 
-private void SetData()
+public class GameManager : MonoBehaviour
 {
-    Player = new Character("Spartan123", 5, 1000);
+    public static GameManager Instance { get; private set; }
+    public Character Player { get; private set; }
 
-    // 아이템 데이터 생성 (아이콘은 Resources 폴더에 있어야 함)
-    Sprite swordIcon = Resources.Load<Sprite>("Icons/sword_icon");
-    Sprite swordIcon = Resources.Load<Sprite>("Icons/sword_icon");
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
 
+        SetData(); // 여기서 호출
+    }
 
-    Player.AddItem(new Item("Iron Sword", swordIcon));
-    Player.AddItem(new Item("Health Potion", potionIcon));
+    private void SetData()
+    {
+        Player = new Character("Spartan123", 5, 1000);
 
-    UIManager.Instance.UIMainMenu.SetCharacter(Player);
-    UIManager.Instance.UIStatus.SetCharacter(Player);
-    UIManager.Instance.UIInventory.InitInventoryUI(Player.Inventory);
+        // 아이템 데이터 생성
+        Sprite swordIcon = Resources.Load<Sprite>("Icons/weapon_lavish_sword");
+        Sprite potionIcon = Resources.Load<Sprite>("Icons/ui_heart_full");
+        Sprite bowIcon = Resources.Load<Sprite>("Icons/weapon_bow");
+        Sprite axeIcon = Resources.Load<Sprite>("Icons/weapon_axe");
+
+        Player.AddItem(new Item("Iavish Sword", swordIcon));
+        Player.AddItem(new Item("Health Potion", potionIcon));
+        Player.AddItem(new Item("Bow", bowIcon));
+        Player.AddItem(new Item("Axe", axeIcon));
+
+        UIManager.Instance.UIMainMenu.SetCharacter(Player);
+        UIManager.Instance.UIStatus.SetCharacter(Player);
+        UIManager.Instance.UIInventory.InitInventoryUI(Player.Inventory);
+    }
 }
